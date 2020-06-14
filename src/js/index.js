af2f7d7bb1b49d6257e9d521982e6df85715f0c4
Mateusz Meler <mateuszmeler@icloud.com>
2020-06-14 20:35:20 +0200

    simple-editor script

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
+
+const entry = localStorage.getItem("entry");
+let result = "";
+
+if (entry) {
+  result = entry;
+}
+
+const textArea = document.querySelector(".area--js");
+textArea.value = result;
+
+const saveButton = document.querySelector(".save--js");
+
+saveButton.addEventListener("click", () => {
+  localStorage.setItem("entry", textArea.value);
+});
+
+const loadButton = document.querySelector(".load--js");
+
+loadButton.addEventListener("click", () => {
+textArea.value = localStorage.getItem("entry", textArea.value);
+});

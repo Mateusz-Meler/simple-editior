5a0ba1c4c3817e6c3ef8a53ce4555e7f56e648f9
Mateusz Meler <mateuszmeler@icloud.com>
2020-06-19 20:54:05 +0200

    js update

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,27 @@
 import "../scss/main.scss";
 
-const entry = localStorage.getItem("entry");
-let result = "";
-
-if (entry) {
-  result = entry;
-}
 
 const textArea = document.querySelector(".area--js");
-textArea.value = result;
-
 const saveButton = document.querySelector(".save--js");
-
-saveButton.addEventListener("click", () => {
-  localStorage.setItem("entry", textArea.value);
-});
-
 const loadButton = document.querySelector(".load--js");
 
-loadButton.addEventListener("click", () => {
-textArea.value = localStorage.getItem("entry", textArea.value);
-});
+const currentValue = localStorage.getItem('entry');
+
+if (currentValue) {
+  alert(`Masz coś w Storage 😎`);
+  document.querySelector('.info--js').innerHTML = 'ℹ️';
+};
+
+saveButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.setItem("entry", textArea.value);
+  if (textArea.value) {
+  document.querySelector('.info--js').innerHTML = "ℹ️"
+} else {
+  document.querySelector('.info--js').innerHTML = ""
+}});
+
+loadButton.addEventListener("click", (e) => {
+  e.preventDefault();
+textArea.value = localStorage.getItem("entry");
+})
